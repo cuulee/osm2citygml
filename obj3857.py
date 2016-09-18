35fec487a1a1a28b600851254ce10ccba751234b
stirringhalo <stirringhalo@users.noreply.github.com>
2016-09-18 10:16:23 -0400

    Major update
    Now flips coords, approximately centers in the right area.

---
+++ obj3857.py
@@ -0,0 +1,33 @@
+from pyproj import Proj, transform
+import sys
+
+input_file=sys.argv[1]
+input_location=sys.argv[2]
+
+inProj=Proj(init='epsg:4326')
+outProj=Proj(init='epsg:3857')
+
+output_file = open("3857_"+input_file,"w")
+
+with open(input_location,"r") as location_file:
+	for line in location_file:
+		line_split=line.split(" ")
+		origin_lat=float(line_split[0])
+		origin_lon=float(line_split[1])
+		origin_x,origin_y=transform(inProj,outProj,origin_lon,origin_lat)
+location_file.close()
+
+with open(input_file,"r") as obj_file:
+	for line in obj_file:
+		if "v " in line:
+			vertex_line=line.split(" ")
+			obj_x=float(vertex_line[1])
+			obj_y=float(vertex_line[2])
+			obj_z=float(vertex_line[3])
+			obj_x=obj_x+origin_x
+			obj_y=obj_y+origin_y
+			output_file.write('v '+str(obj_x)+" "+str(obj_y)+" "+str(obj_z)+"\n")
+		else:
+			output_file.write(line)
+output_file.close()
+obj_file.close()
--- osm2citygml.sh
+++ osm2citygml.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-WAY=$1
+NAME=$1
 
-time curl "https://overpass-api.de/api/interpreter" --data "data=(%0Arel(40.69728274017714%2C-74.01924848556519%2C40.71876991146138%2C-74.00184631347656)%5B%22type%22%3D%22building%22%5D%3B%0Away(40.69728274017714%2C-74.01924848556519%2C40.71876991146138%2C-74.00184631347656)%5B%22building%22%5D%3B%0Away(40.69728274017714%2C-74.01924848556519%2C40.71876991146138%2C-74.00184631347656)%5B%22building%3Apart%22%5D%3B%0A)%3B%0A(._%3B%3E%3B)%3B%0Aout+meta%3B" > $WAY.xml
-time ./osm2world.sh --input $WAY.xml --output $WAY.obj
-time /opt/fme-desktop-2016/fme osm2citygml.fmw  --SourceDataset_OBJ $WAY.obj --DestDataset_CITYGML $WAY.gml
-
+rm $NAME.xml $NAME.gml $NAME.obj $NAME.obj.mtl 3857_$NAME.obj
+echo '*********************************'
+echo 'Getting Data'
+time curl "http://overpass-api.de/api/interpreter" --data "data=(%0Away(40.7028951229916%2C-74.01718854904173%2C40.711922746583795%2C-74.00669574737549)%5B%22building%22%5D%3B%0Away(40.7028951229916%2C-74.01718854904173%2C40.711922746583795%2C-74.00669574737549)%5B%22building%3Apart%22%5D%3B%0A)%3B%0A(._%3B%3E%3B)%3B%0Aout+meta%3B%0A++%0A" > $NAME.xml
+echo '*********************************'
+echo 'Converting to OBJ'
+time ./osm2world.sh --input $NAME.xml --output $NAME.obj
+echo '*********************************'
+echo 'Getting center of OBJ, it is being saved as .obj.loc'
+cat test.obj | grep '#' | grep Coordinate  | cut -d " " -f 6,8 | tr "," " " | tr -s " " > $NAME.obj.loc
+echo '*********************************'
+echo 'Rotating OBJ'
+time /opt/fme-desktop-2016/fme objflip.fmw --SourceDataset_OBJ $NAME.obj --DestDataset_OBJ rot_$NAME
+mv rot_$NAME/$NAME.obj rot_$NAME.obj
+rm -r rot_$NAME
+echo '*********************************'
+echo 'Recentering OBJ into EPSG:3857 coords'
+time python obj3857.py rot_$NAME.obj $NAME.obj.loc
+echo '*********************************'
+echo 'Converting to CityGML'
+time /opt/fme-desktop-2016/fme osm2citygml.fmw --SourceDataset_OBJ 3857_rot_$NAME.obj --DestDataset_CITYGML $NAME.gml
